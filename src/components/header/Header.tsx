f918a362405c7e193c9ed41313833e4a2018a69a
unaldutlu <unaldutlu@gmail.com>
2023-01-30 00:32:27 +0300

    update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import Medicine from '../../assets/images/Medicine.svg'
-import {AiOutlineSearch} from 'react-icons/ai';
+import { AiOutlineSearch } from 'react-icons/ai';
+import Card from './Card';
+import User from './User';
 
 function Header() {
+  window.addEventListener("scroll", function () {
+    const header = this.document.querySelector(".header")
+    //    @ts-ignore
+    header.classList.toggle("active", this.window.scrollY > 100)
+  })
+  window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
   return (
-    <div>Header</div>
+    <>
+      <header className='header'>
+        <div className='scontainer flex'>
+          <div className='logo'>
+            <img src={Medicine} alt="Medicine" />
+          </div>
+          <div className='search flex'>
+            <AiOutlineSearch className='searchIcon' />
+            <input type="text" placeholder='Search...' />
+          </div>
+          <div className="account flexCenter">
+            <Card />
+            <User />
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
 
