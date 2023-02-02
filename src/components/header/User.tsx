6bdd220c7cd2ee6987232eb03184c51e720acf92
unaldutlu <unaldutlu@gmail.com>
2023-02-02 23:53:33 +0300

    add user install-2 on page

--- src/components/header/User.tsx
+++ src/components/header/User.tsx
@@ -9,8 +9,8 @@ import { Link } from "react-router-dom"
 // import { authActions } from "../../store/authSlice"
 
 function User() {
-  // const user = true
-  // const [profileOpen, setProfileOpen] = useState(false)
+  const user = true
+  const [profileOpen, setProfileOpen] = useState(false)
 
   // const close = () => {
   //   setProfileOpen(null)
@@ -23,56 +23,56 @@ function User() {
   return (
     <>
       <div className='profile'>
-        {/* {user ? ( */}
-        <>
-          {/* <button className='img' onClick={() => setProfileOpen(!profileOpen)}>
+        {user ? (
+          <>
+            <button className='img' onClick={() => setProfileOpen(!profileOpen)}>
               <img src='https://cdn-icons-png.flaticon.com/512/2202/2202112.png' alt='' />
-            </button> */}
+            </button>
 
-          {/* {profileOpen && ( */}
-          <div className='openProfile boxItems' onClick={close}>
-            <div className='image'>
-              <Link to='/account'>
-                <div className='img'>
-                  <img src='https://cdn-icons-png.flaticon.com/512/2202/2202112.png' alt='' />
+            {profileOpen && (
+              <div className='openProfile boxItems' onClick={close}>
+                <div className='image'>
+                  <Link to='/account'>
+                    <div className='img'>
+                      <img src='https://cdn-icons-png.flaticon.com/512/2202/2202112.png' alt='' />
+                    </div>
+                  </Link>
+                  <div className='text'>
+                    <h4>Eden Smith</h4>
+                    <label htmlFor=''>Los Angeles,CA</label>
+                  </div>
                 </div>
-              </Link>
-              <div className='text'>
-                <h4>Eden Smith</h4>
-                <label htmlFor=''>Los Angeles,CA</label>
+                <Link to='/login'>
+                  <button className='box'>
+                    <IoSettingsOutline className='icon' />
+                    <h4>My Account</h4>
+                  </button>
+                </Link>
+                <button className='box'>
+                  <BsBagCheck className='icon' />
+                  <h4>My Order</h4>
+                </button>
+                <button className='box'>
+                  <AiOutlineHeart className='icon' />
+                  <h4>Wishlist</h4>
+                </button>
+                <button className='box'>
+                  <GrHelp className='icon' />
+                  <h4>Help</h4>
+                </button>
+                <button className='box' onClick={logoutHandler}>
+                  <BiLogOut className='icon' />
+                  <h4>Log Out</h4>
+                </button>
               </div>
-            </div>
-            <Link to='/login'>
-              <button className='box'>
-                <IoSettingsOutline className='icon' />
-                <h4>My Account</h4>
-              </button>
-            </Link>
-            <button className='box'>
-              <BsBagCheck className='icon' />
-              <h4>My Order</h4>
-            </button>
-            <button className='box'>
-              <AiOutlineHeart className='icon' />
-              <h4>Wishlist</h4>
-            </button>
-            <button className='box'>
-              <GrHelp className='icon' />
-              <h4>Help</h4>
-            </button>
-            <button className='box' onClick={logoutHandler}>
-              <BiLogOut className='icon' />
-              <h4>Log Out</h4>
-            </button>
-          </div>
-          {/* )} */}
-          {/* </> */}
-          {/* ) : ( */}
+            )}
+          </>
+        ) : (
           <button>My Account</button>
-          {/* )} */}
-          {/* </div> */}
-        </>
-        )
+        )}
+      </div>
+    </>
+  )
 }
 
-        export default User
+export default User
