c4e2da1c4efb5b462b2257cd57598d46d70514a5
unaldutlu <unaldutlu@gmail.com>
2023-02-05 23:49:13 +0300

    add home.tsx update on page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react"
+// import { Category } from "../../components/category/Category"
+// import { Order } from "../../components/hero/Order"
+import Slider from "../../components/hero/Slider"
+// import { Product } from "../../components/product/Product"
 
 function Home() {
 
   return (
-    <div>Home</div>
+    <>
+      <Slider />
+      {/* <Order />
+      <Category />
+      <Product /> */}
+    </>
   )
 }
 
